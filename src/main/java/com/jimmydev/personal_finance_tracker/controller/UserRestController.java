18658fcf6b5d0c81f66d93586787001a077ed22e
jimmy <jimoseipeace2bb@gmail.com>
2025-06-11 17:10:51 -0500

    done working on the userController. Close #7

--- src/main/java/com/jimmydev/personal_finance_tracker/controller/UserRestController.java
+++ src/main/java/com/jimmydev/personal_finance_tracker/controller/UserRestController.java
@@ -1,17 +1,56 @@
 package com.jimmydev.personal_finance_tracker.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jimmydev.personal_finance_tracker.dto.UserDto.UserRequestDto;
+import com.jimmydev.personal_finance_tracker.dto.UserDto.UserResponseDto;
+import com.jimmydev.personal_finance_tracker.services.serviceInterfaces.IService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@Validated
 public class UserRestController {
+    private final IService userService;
+
 
 
     @GetMapping("/user")
     String getUser(){
+
         return "Hello User";
     }
+
+    /// Create users
+    @PostMapping("/user")
+    public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserRequestDto userRequestDto){
+        var user = userService.save(userRequestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+
+    }
+
+
+    /// get all users
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponseDto>> getAllUsers(){
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    /// get a specific user
+    @GetMapping("/user/{id}")
+    public  ResponseEntity<UserResponseDto> getUser(@Valid @PathVariable Long id){
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+    }
+
 }
--- src/main/java/com/jimmydev/personal_finance_tracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/jimmydev/personal_finance_tracker/exceptions/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.jimmydev.personal_finance_tracker.exceptions;
 
 
 import com.jimmydev.personal_finance_tracker.dto.ExceptionDto.ErrorResponseDto;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +14,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
@@ -56,17 +59,30 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    /// Here I will handle method argument exceptions
+    /// Here I will handle method argument exceptions for example with the @Valid
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex){
         log.error("Validation exception occurred: {} ", ex.getMessage());
         Map<String,String> errors = new HashMap<>();
-
         ex.getBindingResult().getFieldErrors().forEach(error -> {
             errors.put(error.getField(), error.getDefaultMessage());
         });
         return ResponseEntity.badRequest().body(errors);
     }
 
+    /// Handles the class level exception such as the @Validated
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, List<String>>> handleConstraintViolation(ConstraintViolationException ex){
+        log.error("Validation exception occurred: {} ", ex.getMessage());
+        List<String> errors = ex
+                .getConstraintViolations()
+                .stream()
+                .map(v -> v.getPropertyPath() + ": " + v.getMessage())
+                .toList();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("errors", errors));
+    }
 
 }
